36f0ab68d9d40084455669ca788b3bb1b6fe8328
bayuevri <bayuevri@student.telkomuniversity.ac.id>
2022-03-05 18:14:11 +0700

    Add project files.

---
+++ modul-1302204069/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace modul_1302204069
+{
+    partial class Form1
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.btn_num_1 = new System.Windows.Forms.Button();
+            this.btn_num_2 = new System.Windows.Forms.Button();
+            this.btn_num_3 = new System.Windows.Forms.Button();
+            this.btn_num_6 = new System.Windows.Forms.Button();
+            this.btn_num_5 = new System.Windows.Forms.Button();
+            this.btn_num_4 = new System.Windows.Forms.Button();
+            this.btn_num_9 = new System.Windows.Forms.Button();
+            this.btn_num_8 = new System.Windows.Forms.Button();
+            this.btn_num_7 = new System.Windows.Forms.Button();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.btn_num_0 = new System.Windows.Forms.Button();
+            this.btn_operation_plus = new System.Windows.Forms.Button();
+            this.label_calculation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_num_1
+            // 
+            this.btn_num_1.Location = new System.Drawing.Point(32, 91);
+            this.btn_num_1.Name = "btn_num_1";
+            this.btn_num_1.Size = new System.Drawing.Size(75, 35);
+            this.btn_num_1.TabIndex = 0;
+            this.btn_num_1.Text = "1";
+            this.btn_num_1.UseVisualStyleBackColor = true;
+            this.btn_num_1.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_num_2
+            // 
+            this.btn_num_2.Location = new System.Drawing.Point(124, 91);
+            this.btn_num_2.Name = "btn_num_2";
+            this.btn_num_2.Size = new System.Drawing.Size(75, 35);
+            this.btn_num_2.TabIndex = 1;
+            this.btn_num_2.Text = "2";
+            this.btn_num_2.UseVisualStyleBackColor = true;
+            this.btn_num_2.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_num_3
+            // 
+            this.btn_num_3.Location = new System.Drawing.Point(214, 91);
+            this.btn_num_3.Name = "btn_num_3";
+            this.btn_num_3.Size = new System.Drawing.Size(75, 35);
+            this.btn_num_3.TabIndex = 2;
+            this.btn_num_3.Text = "3";
+            this.btn_num_3.UseVisualStyleBackColor = true;
+            this.btn_num_3.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_num_6
+            // 
+            this.btn_num_6.Location = new System.Drawing.Point(214, 143);
+            this.btn_num_6.Name = "btn_num_6";
+            this.btn_num_6.Size = new System.Drawing.Size(75, 35);
+            this.btn_num_6.TabIndex = 5;
+            this.btn_num_6.Text = "6";
+            this.btn_num_6.UseVisualStyleBackColor = true;
+            this.btn_num_6.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_num_5
+            // 
+            this.btn_num_5.Location = new System.Drawing.Point(124, 143);
+            this.btn_num_5.Name = "btn_num_5";
+            this.btn_num_5.Size = new System.Drawing.Size(75, 35);
+            this.btn_num_5.TabIndex = 4;
+            this.btn_num_5.Text = "5";
+            this.btn_num_5.UseVisualStyleBackColor = true;
+            this.btn_num_5.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_num_4
+            // 
+            this.btn_num_4.Location = new System.Drawing.Point(32, 143);
+            this.btn_num_4.Name = "btn_num_4";
+            this.btn_num_4.Size = new System.Drawing.Size(75, 35);
+            this.btn_num_4.TabIndex = 3;
+            this.btn_num_4.Text = "4";
+            this.btn_num_4.UseVisualStyleBackColor = true;
+            this.btn_num_4.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_num_9
+            // 
+            this.btn_num_9.Location = new System.Drawing.Point(214, 195);
+            this.btn_num_9.Name = "btn_num_9";
+            this.btn_num_9.Size = new System.Drawing.Size(75, 35);
+            this.btn_num_9.TabIndex = 8;
+            this.btn_num_9.Text = "9";
+            this.btn_num_9.UseVisualStyleBackColor = true;
+            this.btn_num_9.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_num_8
+            // 
+            this.btn_num_8.Location = new System.Drawing.Point(124, 195);
+            this.btn_num_8.Name = "btn_num_8";
+            this.btn_num_8.Size = new System.Drawing.Size(75, 35);
+            this.btn_num_8.TabIndex = 7;
+            this.btn_num_8.Text = "8";
+            this.btn_num_8.UseVisualStyleBackColor = true;
+            this.btn_num_8.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_num_7
+            // 
+            this.btn_num_7.Location = new System.Drawing.Point(32, 195);
+            this.btn_num_7.Name = "btn_num_7";
+            this.btn_num_7.Size = new System.Drawing.Size(75, 35);
+            this.btn_num_7.TabIndex = 6;
+            this.btn_num_7.Text = "7";
+            this.btn_num_7.UseVisualStyleBackColor = true;
+            this.btn_num_7.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(214, 246);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(75, 35);
+            this.btn_calculate.TabIndex = 11;
+            this.btn_calculate.Text = "=";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // btn_num_0
+            // 
+            this.btn_num_0.Location = new System.Drawing.Point(124, 246);
+            this.btn_num_0.Name = "btn_num_0";
+            this.btn_num_0.Size = new System.Drawing.Size(75, 35);
+            this.btn_num_0.TabIndex = 10;
+            this.btn_num_0.Text = "0";
+            this.btn_num_0.UseVisualStyleBackColor = true;
+            this.btn_num_0.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_operation_plus
+            // 
+            this.btn_operation_plus.Location = new System.Drawing.Point(32, 246);
+            this.btn_operation_plus.Name = "btn_operation_plus";
+            this.btn_operation_plus.Size = new System.Drawing.Size(75, 35);
+            this.btn_operation_plus.TabIndex = 9;
+            this.btn_operation_plus.Text = "+";
+            this.btn_operation_plus.UseVisualStyleBackColor = true;
+            this.btn_operation_plus.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // label_calculation
+            // 
+            this.label_calculation.AutoSize = true;
+            this.label_calculation.Location = new System.Drawing.Point(29, 44);
+            this.label_calculation.Name = "label_calculation";
+            this.label_calculation.Size = new System.Drawing.Size(0, 16);
+            this.label_calculation.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(323, 324);
+            this.Controls.Add(this.label_calculation);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.btn_num_0);
+            this.Controls.Add(this.btn_operation_plus);
+            this.Controls.Add(this.btn_num_9);
+            this.Controls.Add(this.btn_num_8);
+            this.Controls.Add(this.btn_num_7);
+            this.Controls.Add(this.btn_num_6);
+            this.Controls.Add(this.btn_num_5);
+            this.Controls.Add(this.btn_num_4);
+            this.Controls.Add(this.btn_num_3);
+            this.Controls.Add(this.btn_num_2);
+            this.Controls.Add(this.btn_num_1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btn_num_1;
+    private System.Windows.Forms.Button btn_num_2;
+    private System.Windows.Forms.Button btn_num_3;
+    private System.Windows.Forms.Button btn_num_6;
+    private System.Windows.Forms.Button btn_num_5;
+    private System.Windows.Forms.Button btn_num_4;
+    private System.Windows.Forms.Button btn_num_9;
+    private System.Windows.Forms.Button btn_num_8;
+    private System.Windows.Forms.Button btn_num_7;
+    private System.Windows.Forms.Button btn_calculate;
+    private System.Windows.Forms.Button btn_num_0;
+    private System.Windows.Forms.Button btn_operation_plus;
+    private System.Windows.Forms.Label label_calculation;
+}
+}
+
---
+++ modul-1302204069/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modul_1302204069
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_num_Click(object sender, EventArgs e)
+        {
+            // Mendapatkan angka dari tombol yang ditekan
+            Button btn = (Button)sender;
+            string stringNumber = btn.Text;
+
+            // Cek jika operator duplikat dan invalid
+            if (!isDuplicateOperator(stringNumber) && !isInvalid(stringNumber))
+            {
+                // Menampilkan tombol yang ditekan
+                this.label_calculation.Text += stringNumber;
+            }
+
+            // Method untuk mengecek operator ditulis sebagai huruf pertama
+            Boolean isInvalid(string num)
+            {
+                string label = this.label_calculation.Text;
+                int length = label.Length;
+
+                return (length == 0 && num.Equals("+"));
+            }
+
+            // Method untuk mengecek operator duplikat
+            // Contoh: 123 + 32 ++ 232
+            Boolean isDuplicateOperator(string num)
+            {
+                string label = this.label_calculation.Text;
+                int length = label.Length;
+
+                if (length > 0 && num.Equals("+"))
+                {
+                    return getLastChar(label) == '+';
+                }
+
+                return false;
+            }
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            // Cek jika operator berada di akhir kalimat matematika.
+            if (isInvalid()) return;
+
+            // Mengambil teks label
+            string stringCalculate = this.label_calculation.Text;
+
+            // Memisahkan tiap angka dengan operator '+'
+            string[] stringCalculateSplit = stringCalculate.Split('+');
+
+            // Menjumlahkan tiap angka dari field split
+            int calculate = 0;
+            for (int i = 0; i < stringCalculateSplit.Length; i++)
+            {
+                calculate += Convert.ToInt32(stringCalculateSplit[i]);
+            }
+
+            // Menampilkan hasil hitung dari field calculate
+            this.label_calculation.Text = calculate.ToString();
+
+            // Method untuk mengecek operator berada diakhir kalimat matematika
+            // Contoh: 12 +
+            Boolean isInvalid()
+            {
+                return getLastChar(this.label_calculation.Text) == '+';
+            }
+        }
+
+        private char getLastChar(string text)
+        {
+            return text[text.Length - 1];
+        }
+    }
+}
